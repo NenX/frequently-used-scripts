3bc8195afbc1df99ddd9c25069b031a387583157
noah <noah1999@qq.com>
2022-04-03 01:00:11 +0800

    feat: generate ssh key for github

---
+++ ssh/gen-ssh-key-for-github.sh
@@ -0,0 +1,39 @@
+# for: getUserInput echoBLue echoGreen
+source utils/index.sh
+
+main()
+{
+    getUserInput "enter keyname:" "newKeyname"
+    filename=id_rsa_git_$value
+
+    getUserInput "enter email:" "newEmail@qq.com"
+    email=$value
+
+    getUserInput "enter aliasname:" "alias.github.com"
+    aliasname=$value
+
+    getUserInput "enter hostname:" "github.com"
+    hostname=$value
+
+    # getUserInput "enter port:" "22"
+    # port=$value
+
+    ssh-keygen -t rsa -C $email -f "$HOME/.ssh/$filename"
+    
+    echo -e "\n
+Host $aliasname
+    HostName $hostname
+    # Port $port
+    User git
+    IdentityFile ~/.ssh/$filename" >> ~/.ssh/config
+
+    echoBlue "append the following pub key to your GitHub SSH keys:"
+
+    cat ~/.ssh/$filename.pub
+
+    echoGreen "useage: git clone git@$aliasname:NenX/frequently-used-scripts.git"
+
+}
+ 
+
+main;
